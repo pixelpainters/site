8e5591212947076edd9ed17007d6508a9762ddcc
Sanju Rao <sanjurao@Sanjus-Mac.local>
2020-06-21 01:18:53 +0530

    Pricing page fix

--- js/script.js
+++ js/script.js
@@ -34,8 +34,7 @@ function tabfn(evt, cityName) {
 $(document).ready(function() {
     $(".owl-carousel").owlCarousel();
 
-});
-$('#profile_slide').owlCarousel({
+    $('#profile_slide').owlCarousel({
         loop: true,
         margin: 10,
         nav: true,
@@ -72,8 +71,77 @@ $('#profile_slide').owlCarousel({
             }
         }
     })
+});
 /**-------menu ------------ */
 function menuToggle() {
     var element = document.getElementById("nav_bar");
     element.classList.toggle("menu");
 }
+
+
+/**------owl carosel------- */
+$(document).ready(function() {
+    var $filterCheckboxes = $('input.price-filter');
+    
+    console.log($filterCheckboxes);
+    
+
+    // Read All Available Filter Groups
+    var allFilters = [];
+    $filterCheckboxes.each(function() {
+        if ($.inArray(this.name,allFilters) == -1){
+          allFilters.push(this.name);
+      }  
+    });
+
+  $filterCheckboxes.on('change', function() {
+  
+    // create a collection containing all of the filterable elements
+    var $filteredResults = $('.paint-items');
+  
+      var $filterCategoryApplied = 0;
+    
+    $.each(allFilters, function(arIndex, filterName) {
+      var $filterCheckboxCategory = $('input[name='+filterName+']').filter(':checked');
+           
+     if ( $filterCheckboxCategory.length === 0 ) {
+      $filteredResults = [];
+     }
+    });
+  
+      // Read Selectetd Filters
+    var selectedFilters = {};
+    $filterCheckboxes.filter(':checked').each(function() {
+      if (!selectedFilters.hasOwnProperty(this.name)) {
+        selectedFilters[this.name] = [];
+      }
+      selectedFilters[this.name].push(this.value);
+    });
+    
+  
+    // loop over the selected filter name -> (array) values pairs
+    $.each(selectedFilters, function(name, filterValues) {
+
+      // filter each .flower element
+      $filteredResults = $filteredResults.filter(function() {
+  
+        var matched = false,
+          currentFilterValues = $(this).data('category').split(' ');
+  
+        // loop over each category value in the current .flower's data-category
+        $.each(currentFilterValues, function(_, currentFilterValue) {
+          if ($.inArray(currentFilterValue, filterValues) != -1) {
+            matched = true;
+            return false;
+          }
+        });
+        return matched;
+  
+      });
+    });
+  
+    $('.paint-items').hide().filter($filteredResults).show();
+  
+  });
+  
+});
